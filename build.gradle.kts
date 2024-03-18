c8b2fff3d6a280bc5b02ba47538be33cfa6e7a22
Wojturski Maciej <w65578@student.wsiz.edu.pl>
2024-03-18 10:13:46 +0100

    Well... I had tried to add dependency code generation from swagger for 2 or 3 days. I think it's work now, but after run program, console shows only logo from spring. Request from PostMan works so we have progress.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.flywaydb.flyway") version "10.10.0"
+    id("org.openapi.generator") version "5.3.0"
 }
-
-apply(plugin = "org.flywaydb.flyway")
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -37,17 +37,25 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//    implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core:9.16.3")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
     implementation ("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.swagger.codegen.v3:swagger-codegen-generators:1.0.47") {
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+    }
+    // most of the dependencies above below are important for swagger codegen
+    implementation("io.gsonfire:gson-fire:1.9.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.threeten:threetenbp:1.6.8")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    testImplementation("junit:junit:4.12")
     implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
 }
@@ -59,3 +67,30 @@ tasks.withType<Test> {
 tasks.bootBuildImage {
     builder.set("paketobuildpacks/builder-jammy-base:latest")
 }
+
+ openApiGenerate {
+     configOptions.set(mapOf(
+             "generateModelTests" to "false",
+             "generateApiTests" to "false",
+     ))
+     generatorName.set("java")
+     inputSpec.set("A:\\Projekty\\WorkoutTracker\\src\\main\\resources\\contract\\contract.yml")
+     outputDir.set("A:\\Projekty\\WorkoutTracker\\build\\generated-sources\\swagger")
+     apiPackage.set("com.example.api")
+     modelPackage.set("com.example.model")
+     invokerPackage.set("com.example.invoker")
+
+ }
+
+ sourceSets {
+     main {
+         java {
+             srcDir("A:\\Projekty\\WorkoutTracker\\build\\generated-sources\\swagger\\main")
+         }
+     }
+     test {
+         java {
+             srcDir("A:\\Projekty\\WorkoutTracker\\build\\generated-sources\\swagger\\test")
+         }
+     }
+ }
