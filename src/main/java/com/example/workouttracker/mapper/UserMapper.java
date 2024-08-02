805cb585705434430b4cd0bccecca3912d89860a
wojtur <mackowski28@gmail.com>
2024-08-02 09:43:35 +0200

    Added mapper for String to Set and revers

--- src/main/java/com/example/workouttracker/mapper/UserMapper.java
+++ src/main/java/com/example/workouttracker/mapper/UserMapper.java
@@ -3,21 +3,43 @@ package com.example.workouttracker.mapper;
 import com.example.model.User;
 import com.example.model.UserDetails;
 import com.example.workouttracker.core.user.UserEntity;
+import com.example.workouttracker.core.user.UserGender;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "genders", source = "genders", qualifiedByName = "mapGendersToString")
+    UserDetails toDetailsDto(UserEntity userEntity);
+
+    @Mapping(target = "genders", source = "genders", qualifiedByName = "mapStringToGenders")
+    UserEntity toEntity(UserDetails userDetails);
+
+    @Mapping(target = "genders", source = "genders", qualifiedByName = "mapGendersToString")
     User toDto(UserEntity userEntity);
 
+    @Mapping(target = "genders", source = "genders", qualifiedByName = "mapStringToGenders")
     UserEntity toEntity(User user);
 
     List<User> toDto(List<UserEntity> userEntities);
 
-    UserDetails toDetailsDto(UserEntity userEntity);
+    @Named("mapGendersToString")
+    default String mapGendersToString(Set<UserGender> genders) {
+        return genders.stream().map(Enum::name).collect(Collectors.joining(","));
+    }
 
-    UserEntity toEntity(UserDetails userDetails);
-
+    @Named("mapStringToGenders")
+    default Set<UserGender> mapStringToGenders(String genders) {
+        return Arrays.stream(genders.split(","))
+                .map(UserGender::valueOf)
+                .collect(Collectors.toSet());
+    }
 }
